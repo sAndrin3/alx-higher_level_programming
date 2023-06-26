1bccb889275d626767af2291e281dce5bb8e7cda
sAndrin3 <sandrinemalanga2001@gmail.com>
2023-06-26 10:06:33 -0700

    0-safe_print_list.py

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,24 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a doubly linked list.
+ * print_dlistint - Prints all the elements of a dlistint_t list.
  * @h: Pointer to the head of the list.
- * Return: Number of nodes in the list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current;
     size_t count = 0;
 
-    if (h == NULL)
-        return 0;
-
-    current = h;
-    while (current != NULL)
+    while (h != NULL)
     {
-        printf("%d\n", current->n);
+        printf("%d\n", h->n);
+        h = h->next;
         count++;
-        current = current->next;
     }
 
     return count;
